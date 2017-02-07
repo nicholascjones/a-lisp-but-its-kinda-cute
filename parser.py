b7fc5a82dbac9e225d8aa2ee8538c340413c9106
Nicholas Jones <njones7@nd.edu>
2017-02-07 14:56:15 -0500

    initialized OOP format

--- parser.py
+++ parser.py
@@ -1,21 +1,30 @@
 from string import *
 
+
+class ParseNode:
+
+	def __init__(self, txt, left=None, right=None, tokens=None):
+		self.txt = txt
+		self.left = left
+		self.right = right
+		self.tokens = set(['+','-','/','*','<','>','?','(',')'])
+
+
+
+	def read_text_format(self):
+		tokenized = []
+		self.txt = self.txt.replace('(',' ( ')
+		self.txt = self.txt.replace(')',' ) ')
+		for term in self.txt.split():
+			if term in self.tokens or term.isalpha() or term.isdigit(): #checks for validity
+				tokenized.append(term)
+
+		return tokenized
+
+
 t1 = '(hey guys (j) sup )'
 
 
-t = ['+','-','/','*','<','>','?','(',')']
-tokens = set(t)
+p = ParseNode(t1)
 
-def read_text_format(txt):
-	parseTree = []
-	txt = txt.replace('(',' ( ')
-	txt = txt.replace(')',' ) ')
-	for term in txt.split():
-		if term in tokens or term.isalpha() or term.isdigit(): #checks for validity
-			parseTree.append(term)
-
-	return parseTree
-
-
-
-print read_text(t1)
+print p.read_text_format()
